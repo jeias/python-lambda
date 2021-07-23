3a945c24dd1de9af6ddfe4d82ec53b84903ad14d
Jeias Soares <jeias.soares@zup.com.br>
2021-07-23 17:29:49 -0300

    get item

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,10 @@ import json
 def lambda_handler(event, context):
     # TODO implement
 
+    print("body -> " + event.get('body'))
+    print("direct message ->" + event.get('message'))
+
     return {
         'statusCode': 200,
-        'body': json.dumps(event['body'])
+        'body': json.dumps({'top': "eu"})
     }
