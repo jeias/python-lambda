df9a4d1b327b9016386b44a9f40bd978775bc16d
Jeias Soares <jeias.soares@zup.com.br>
2021-07-23 09:54:52 -0300

    adding body

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,8 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
+
     return {
         'statusCode': 200,
-        'body': json.dumps('MUDEI')
+        'message': event['message']
     }
