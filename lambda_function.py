5e94770c60b7293bea3f2748355d6d7534162000
Jeias Soares <jeias.soares@zup.com.br>
2021-07-23 17:05:52 -0300

    vai tudo

--- lambda_function.py
+++ lambda_function.py
@@ -4,13 +4,7 @@ import json
 def lambda_handler(event, context):
     # TODO implement
 
-    coco = 'coco'
-
-    if event['message']:
-        coco = event['message']
     return {
         'statusCode': 200,
-        'body': {
-            'message': coco
-        }
+        'body': event
     }
