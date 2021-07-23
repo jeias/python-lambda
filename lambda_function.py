163d721612bf99fcabc27241d584f88935b08759
Jeias Soares <jeias.soares@zup.com.br>
2021-07-23 14:57:47 -0300

    roll back

--- lambda_function.py
+++ lambda_function.py
@@ -3,8 +3,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-
     return {
         'statusCode': 200,
-        'message': 'ta dificil e nao eh pouco'
+        'body': json.dumps('Hello from Lambda!')
     }
